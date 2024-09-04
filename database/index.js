64e2f05bb32c3177f73b3e6ad2c469676f7bff92
ewehiuw3743283478 <159542016+ewehiuw3743283478@users.noreply.github.com>
2024-09-04 22:08:14 +0800

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,21 +1,34 @@
 "use strict";
-const Database=require("better-sqlite3");
-module.exports=(conf={})=>{
-var {path=__dirname+'/db.db'}=conf;
-var DB=new Database(path);
+const Database = require("better-sqlite3");
 
-const {servers}=require("./servers")(DB),
-    {traffic,lt}=require("./traffic")(DB),
-    {load_m,load_h}=require("./load")(DB),
-    {ssh_scripts}=require("./ssh_scripts")(DB),
-    {setting}=require("./setting")(DB);
-function getServers(){return servers.all();}
-return {
-    DB,
-    servers,getServers,
-    traffic,lt,
-    load_m,load_h,
-    ssh_scripts,
-    setting,
+module.exports = (conf = {}) => {
+    // Extract configuration path or use default
+    const { path = __dirname + '/db.db' } = conf;
+    // Initialize the SQLite database
+    const DB = new Database(path);
+
+    // Import and initialize database tables and related functionality
+    const { servers } = require("./servers")(DB);
+    const { traffic, lt } = require("./traffic")(DB);
+    const { load_m, load_h } = require("./load")(DB);
+    const { ssh_scripts } = require("./ssh_scripts")(DB);
+    const { setting } = require("./setting")(DB);
+
+    // Define a function to get all servers
+    function getServers() {
+        return servers.all();
+    }
+
+    // Return an object with all functionalities and the database instance
+    return {
+        DB,
+        servers,
+        getServers,
+        traffic,
+        lt,
+        load_m,
+        load_h,
+        ssh_scripts,
+        setting,
+    };
 };
-}
